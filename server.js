5fd594458493fa210b587ffef0d917d576c2f028
Leonardo Oliveira <leonardo.viegaso@gmail.com>
2026-08-06 20:51:48 -0300

    fix tamanho do json

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.set('trust proxy', true);
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Caminho onde o arquivo de dados JSON será salvo no seu servidor Linux
 const DATA_FILE = process.env.DATA_PATH || path.join(__dirname, 'chamados.json');
